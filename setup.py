9a2ce78f9d4dc38478607a7bf1212474372ef143
Zachary Zeng <48763165+Ch33kiBr33ki@users.noreply.github.com>
2021-04-11 14:41:18 +0800

    version1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="MyLibrary",
+    version="1.0",
+    install_requires=[
+        "requests",
+        "xlwt",
+        "urllib",
+        "pandas",
+        "bs4",
+    ],
+)
